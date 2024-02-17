71ac8a8dafcdf208a645fd96d0680bdfa2ecb701
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-17 17:50:11 +0100

    add collegamento con l'api e chiamata di prova

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,62 @@
 <script>
-import AppTest from './components/AppTest.vue'
-
-
+import{store} from './store';
+import AppTest from './components/AppTest.vue';
+import axios from 'axios';
+ 
 export default {
   data() {
+    
     return {
-      
+      store,
+      search:'fast and furios',
+      filmList:[],
     }
   },
 
 
   components: { AppTest },
+  methods:{
+    searchform(){
+      axios.get('https://api.themoviedb.org/3/search/movie', 
+      
+      {
+        params:{
+          api_key:'4c1d191bb7a905f2c05cb735ff5833f5',
+          query: this.search,
+        }
+        
+      }
+      
+      )
+      .then((resp) => {
+        this.filmList = resp.data.results;
+        
+        console.log(resp.data.results);
+        console.log(this.filmList);
+
+      });
+    },
+
+  },
+    
 };
 
 </script>
 
 
 <template>
-  <h1 class="text-center mt-5">BooFlix</h1>
+  <div class="container mt-5">
+    <div class=" d-flex">
+      <input type="text" class="form-control" v-model="search">
+      <button @click="searchform()" class="btn btn-primary">cerca</button>
+    </div>
+
+    <ul>
+      <li v-for="film in filmList">
+        {{ film.title}}
+      </li>
+    </ul>
+  </div>
   <AppTest/>
   
 </template>
