33684601dae678e399638c6ef8369d90c7727e0c
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-17 20:19:14 +0100

    add : chiamata multi e  func per vedere le pagine succssive

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,15 @@ export default {
     <div class=" d-flex">
       <input type="text" class="form-control" v-model="store.search">
       <button @click="store.searchform()" class="btn btn-primary">cerca</button>
+      <button @click="store.nextPage()" class="btn btn-success">next page</button>
     </div>
 
     <ul>
       
       <li v-for="film in store.filmList">
-        {{ film.title}}
+        <span class="text-primary" v-if="film.media_type == 'movie'">tittolo film : {{ film.title }} tipo:{{film.media_type}}</span>
+        <span class="text-success" v-if="film.media_type == 'tv'">tittolo serie : {{ film.original_name }} tipo:{{film.media_type}}</span>
+
       </li>
     </ul>
   </div>
--- src/store/index.js
+++ src/store/index.js
@@ -2,26 +2,34 @@
 import { reactive }  from 'vue';
 import axios from 'axios';
 // credenziali per la chiamata api
-const apiUri = 'https://api.themoviedb.org/3/search/movie';
+const apiUri = 'https://api.themoviedb.org/3/search/multi';
 const apiKey = '4c1d191bb7a905f2c05cb735ff5833f5';
 
 
 export const store = reactive({
-    
-    search:'fast and furios',
+    currentPage:1,
+    search:'fast',
     filmList:[],
-    
+    nextPage(){
+        this.currentPage ++;
+        console.log('CURRENTPAGE: '+this.currentPage);
+        this.searchform();
+    },
     searchform(){
         axios.get(apiUri,{
         params:{
             api_key: apiKey,
             query: this.search,
+            page:this.currentPage
         }})
 
-        .then((resp) => {
+        .then((resp) => {  
         this.filmList = resp.data.results;
-        
         console.log(resp.data.results);
+        console.log(resp.data.total_pages);
+        console.log(resp.data.total_results);
+
+
         console.log(this.filmList);
 
         });
